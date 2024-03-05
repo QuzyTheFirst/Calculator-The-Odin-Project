1e4df2378e9aca9e0986c2ccf1812d8c61865dfa
Quzy <40630448+QuzyTheFirst@users.noreply.github.com>
2024-03-05 20:56:28 +0000

    Created UI for calculator

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const buttonOne = document.querySelector(".button-one");
+const buttonTwo = document.querySelector(".button-two");
+const buttonThree = document.querySelector(".button-three");
+const buttonFour = document.querySelector(".button-four");
+const buttonFive = document.querySelector(".button-five");
+const buttonSix = document.querySelector(".button-six");
+const buttonSeven = document.querySelector(".button-seven");
+const buttonEight = document.querySelector(".button-eight");
+const buttonNine = document.querySelector(".button-nine");
+
+const buttonPlus = document.querySelector("button-plus");
+const buttonMinus = document.querySelector("button-minus");
+const buttonMultiply = document.querySelector("button-multiply");
+const buttonDivide = document.querySelector("button-divide");
+const buttonClear = document.querySelector("button-clear");
+const buttonBack = document.querySelector("button-back");
